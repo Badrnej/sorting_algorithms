ae6fcbdda57c5582180f09a1a6c875ac46aff151
Badrnej <nejaa.badr@gmail.com>
2024-02-17 03:48:23 +0100

    test4

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,22 +10,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t a, b, tmp;
+  size_t a, b, tmp;
 
-	if (size < 2)
-		return;
+  if (size < 2)
+    return;
 
-	for (a = 0; a <= size - 1; a++)
-	{
-		for (b = 0; b <= size - 2; b++)
-		{
-			if (array[b] > array[b + 1])
-			{
-				tmp = array[b];
-				array[b] = array[b + 1];
-				array[b + 1] = tmp;
-				print_array(array, size);
-			}
-		}
-	}
+  for (a = 0; a <= size - 1; a++)
+  {
+    for (b = 0; b <= size - 2; b++)
+    {
+      if (array[b] > array[b + 1])
+      {
+        tmp = array[b];
+        array[b] = array[b + 1];
+        array[b + 1] = tmp;
+        print_array(array, size);
+      }
+    }
+  }
 }
