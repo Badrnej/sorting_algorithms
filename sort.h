3326887a2be2b10efeeddc380b2d500fd62be394
Badrnej <nejaa.badr@gmail.com>
2024-02-20 07:56:42 -0800

    vf9

--- sort.h
+++ sort.h
@@ -25,6 +25,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+void print_sorted(int *array, int start, int end);
+/* tasks */
 
 /* Sorting Algorithms Functions */
 
@@ -43,5 +45,15 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+
+/* helper functions */
+size_t size_of_list(listint_t *list);
+void swap(int *indx_1, int *indx_2);
+int hoarePartition(int *array, int start, int end, size_t size);
+void quick_sort_helper(int *array, int start, int end, size_t size);
+void merge_sort_helper(int *array, int *aux, int start, int end);
+void merging(int *array, int *aux, int start, int mid, int end);
+void heapify(int *array, size_t n, size_t i);
+void swap_list(listint_t *tmp, listint_t **list);
 
 #endif /* SORT_H */
